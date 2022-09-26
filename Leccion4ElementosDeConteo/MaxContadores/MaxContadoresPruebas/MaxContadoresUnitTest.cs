fcff045b8f32f4a8015d02d9ac6cc914264ff840
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-09-26 16:54:08 -0500

    Correccion Prueba Unitaria

--- Leccion4ElementosDeConteo/MaxContadores/MaxContadoresPruebas/MaxContadoresUnitTest.cs
+++ Leccion4ElementosDeConteo/MaxContadores/MaxContadoresPruebas/MaxContadoresUnitTest.cs
@@ -6,18 +6,18 @@ namespace MaxContadoresPruebas
     public class MaxContadoresUnitTest
     {
         [TestMethod]
-        public void Prueba3446144Regresa4()
+        public void Prueba3446144Regresa32242()
         {
             //Arrange = Arreglar o adecuar la prueba unitaria
-            Algoritmo AlgMaxContadores = new Algoritmo();
+            Algoritmo AlgMaxContadores = new();
             int N = 5;
-            int[] A_evaluada = { 3, 4, 4, 6, 1, 4, 4 };
+            int[] A_evaluada = {3,4,4,6,1,4,4};
             int[] obtenido;
-            int[] esperado = { 3, 2, 2, 4, 2 };
+            int[] esperado = {3,2,2,4,2};
             //Act = Actuar o realizar la prueba unitaria
             obtenido = AlgMaxContadores.ContadorMaximo(N,A_evaluada);
             //Assert = Verificar que la prueba cumplio con la condicion
-            Assert.AreEqual(obtenido, esperado);
+            CollectionAssert.AreEquivalent(obtenido, esperado);
         }
     }
 }
