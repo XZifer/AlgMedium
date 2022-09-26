750b62f894dcde50bd992dc54ec6907957387c66
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-09-26 16:55:04 -0500

    Desarrollando Metodo ContadorMaximo

--- Leccion4ElementosDeConteo/MaxContadores/MaxContadoresBiblioteca/Algoritmo.cs
+++ Leccion4ElementosDeConteo/MaxContadores/MaxContadoresBiblioteca/Algoritmo.cs
@@ -2,9 +2,31 @@
 {
     public class Algoritmo
     {
-        public int[] ContadorMaximo(int N, int[] a_evaluada)
+        //N = Numero de Contadores
+        public int[] ContadorMaximo(int N, int[] A)
         {
-            throw new NotImplementedException();
+            int[] Contador = new int[N];
+            foreach (int op in A)
+            {
+                for (int i = 0; i < Contador.Length; i++)
+                {
+                    if (op == i +1 )
+                    {
+                        Contador[i]++;
+                        i = Contador.Length;
+                    }
+                    else if (op > N)
+                    {
+                        int max = Contador.Max();
+                        for (int c = 0; c < Contador.Length; c++)
+                        {
+                            Contador[c] = max;
+                        }
+                        i = Contador.Length;
+                    }
+                }
+            }
+            return Contador;
         }
     }
 }
