f89ce51dd4aec0fcd2dfd8d50cadf493b9b75613
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-09-26 16:55:39 -0500

    Agregando Codigo Aplicacion de Consola

--- Leccion4ElementosDeConteo/MaxContadores/MaxContadoresAlgoritmo/Program.cs
+++ Leccion4ElementosDeConteo/MaxContadores/MaxContadoresAlgoritmo/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MaxContadoresBiblioteca;
+
+Algoritmo AlgMaxContadores = new();
+int N = 5;
+int[] A= {3,4,4,6,1,4,4};
+int[] obtenido;
+int[] esperado = {3,2,2,4,2};
+obtenido = AlgMaxContadores.ContadorMaximo(N, A);
+Console.WriteLine(obtenido);
+Console.WriteLine(esperado);
+Console.ReadLine();
