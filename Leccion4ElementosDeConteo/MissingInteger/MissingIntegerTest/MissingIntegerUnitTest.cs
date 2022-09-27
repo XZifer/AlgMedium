8e149584b8488f99361e3a58320b851ef183259e
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-09-27 10:53:06 -0500

    Agregando Pruebas Unitarias MissingInteger

--- Leccion4ElementosDeConteo/MissingInteger/MissingIntegerTest/MissingIntegerUnitTest.cs
+++ Leccion4ElementosDeConteo/MissingInteger/MissingIntegerTest/MissingIntegerUnitTest.cs
@@ -1,11 +1,39 @@
+using MissingIntegerLibrary;
+
 namespace MissingIntegerTest
 {
     [TestClass]
     public class MissingIntegerUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Prueba136412Regresa5()
         {
+            Algoritmo algoritmo = new();
+            int[] A_evaluado = {1,3,6,4,1,2};
+            int obtenido;
+            int esperado = 5;
+            obtenido = algoritmo.MissingInt(A_evaluado);
+            Assert.AreEqual(obtenido, esperado);    
+        }
+        [TestMethod]
+        public void Prueba123Regresa4()
+        {
+            Algoritmo algoritmo = new();
+            int[] A_evaluado = { 1,2,3 };
+            int obtenido;
+            int esperado = 4;
+            obtenido = algoritmo.MissingInt(A_evaluado);
+            Assert.AreEqual(obtenido, esperado);
+        }
+        [TestMethod]
+        public void Prueba_1_3Regresa1()
+        {
+            Algoritmo algoritmo = new();
+            int[] A_evaluado = { -1,-3 };
+            int obtenido;
+            int esperado = 1;
+            obtenido = algoritmo.MissingInt(A_evaluado);
+            Assert.AreEqual(obtenido, esperado);
         }
     }
 }
