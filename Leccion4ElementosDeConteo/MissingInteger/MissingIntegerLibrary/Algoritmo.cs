1ba96cb5dc83bf4844a14d405fee579dc3ec73ff
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-09-27 11:24:48 -0500

    Desarrollando Algoritmo MissingInteger

--- Leccion4ElementosDeConteo/MissingInteger/MissingIntegerLibrary/Algoritmo.cs
+++ Leccion4ElementosDeConteo/MissingInteger/MissingIntegerLibrary/Algoritmo.cs
@@ -1,7 +1,38 @@
-﻿namespace MissingIntegerLibrary
+﻿using System.Linq;
+
+namespace MissingIntegerLibrary
 {
     public class Algoritmo
     {
-
+        public int MissingInt(int[] A)
+        {
+            int[] B;
+            int missing = 1;
+            int index;
+            A.Distinct().ToArray();
+            B = A.OrderBy(x => x).ToArray();
+            index = Array.IndexOf(B, 1);
+            if (index != -1)
+            {
+                for (int i = index; i < B.Length; i++)
+                {
+                    if (i == B.Length -1)
+                    {
+                        missing = B[i] + 1;
+                        return missing;
+                    }
+                    else if (B[i + 1] - B[i] > 1)
+                    {
+                        missing = B[i] + 1;
+                        return missing;
+                    }
+                }
+            }
+            else
+            {
+                missing = 1;
+            }
+            return missing;
+        }
     }
 }
