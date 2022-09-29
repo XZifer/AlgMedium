c8985ad46350596e05e5f5cc538e598ae1d4862b
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-09-29 12:50:40 -0500

    Agregando Desarrollo Program.cs Missinng

--- Leccion4ElementosDeConteo/MissingInteger/MissingIntegerAlgoritm/Program.cs
+++ Leccion4ElementosDeConteo/MissingInteger/MissingIntegerAlgoritm/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MissingIntegerLibrary;
+using System.Collections.Generic;
+
+Console.WriteLine("Menor Entero Positivo Faltante (MEPF)");
+Algoritmo algoritmo = new();
+List<int> A = new();
+string opcion = "S";
+int obtenido;
+string agregar;
+Console.WriteLine("¡Desea Buscar el MEPF de una matriz? S-Si Otro-No");
+opcion = Console.ReadLine();
+while (opcion.ToUpper() == "S")
+{
+    foreach (var item in A)
+    {
+        Console.Write("{0} ",item);
+    }
+    Console.WriteLine();
+    Console.WriteLine("Agrega un numero a tu matriz");
+    agregar = Console.ReadLine();
+    int number;
+    bool success = int.TryParse(agregar, out number);
+    if (success)
+    {
+        number = int.Parse(agregar);
+        A.Add(number);
+        Console.WriteLine("¿Agregar otro numero a la matriz?S-Si Otro-No");
+    }
+    else
+    {
+        Console.WriteLine("No es un numero,¿Deseas Agregar un numero?S-Si Otro-No");
+    }
+    opcion = Console.ReadLine();
+    Console.Clear();
+}
+foreach (var item in A)
+{
+    Console.Write("{0} ", item);
+}
+Console.WriteLine();
+int[]B = A.ToArray();
+obtenido = algoritmo.MissingInt(B);
+Console.WriteLine("El numero faltante es : {0}",obtenido);
