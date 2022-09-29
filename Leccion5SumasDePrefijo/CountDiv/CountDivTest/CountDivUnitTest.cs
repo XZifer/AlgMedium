234d03dbfad69ebb7d6e97ebd8afe735142d3429
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-09-29 17:22:08 -0500

    Agregando Pruebas Unitarias CountDivUnitTest

--- Leccion5SumasDePrefijo/CountDiv/CountDivTest/CountDivUnitTest.cs
+++ Leccion5SumasDePrefijo/CountDiv/CountDivTest/CountDivUnitTest.cs
@@ -1,11 +1,21 @@
+using CountDivLibrary;
+
 namespace CountDivTest
 {
     [TestClass]
     public class CountDivUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PruebaA6B11K2Regresa168()
         {
+            Algoritm al = new();
+            int A_evaluado = 6;
+            int B_evaluado = 11;
+            int K_evaluado = 2;
+            int[] esperado = { 6,8,10 };
+            int[] obtenido;
+            obtenido = al.CountDivisible( A_evaluado,B_evaluado,K_evaluado );
+            CollectionAssert.AreEqual( esperado, obtenido );
         }
     }
 }
