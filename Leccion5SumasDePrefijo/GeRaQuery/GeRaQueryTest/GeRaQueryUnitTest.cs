d0fdf9832f33163811c8fb9df66c4c9e5467ab8e
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-09-30 14:10:38 -0500

    Agregando PruebaScagcctaP250Q456Regresa241

--- Leccion5SumasDePrefijo/GeRaQuery/GeRaQueryTest/GeRaQueryUnitTest.cs
+++ Leccion5SumasDePrefijo/GeRaQuery/GeRaQueryTest/GeRaQueryUnitTest.cs
@@ -1,11 +1,21 @@
+using GeRaQueryLibrary;
+
 namespace GeRaQueryTest
 {
     [TestClass]
     public class GeRaQueryUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PruebaScagcctaP250Q456Regresa241()
         {
+            Algoritmo algoritmo = new Algoritmo();
+            string S_evaluado = "CAGCCTA";
+            int[] P_evaluado = { 2, 5, 0 };
+            int[] Q_evaluado = { 4, 5, 6 };
+            int[] obtenido;
+            int[] esperado = { 2, 4, 1 };
+            obtenido = algoritmo.FactMin(S_evaluado,P_evaluado,Q_evaluado);
+            CollectionAssert.AreEqual(obtenido, esperado);
         }
     }
 }
