dada10723fdca4808848f2f803a745e20d2b9ea9
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-09-29 19:22:35 -0500

    Correccion de Algoritm

--- Leccion5SumasDePrefijo/CountDiv/CountDivLibrary/Algoritm.cs
+++ Leccion5SumasDePrefijo/CountDiv/CountDivLibrary/Algoritm.cs
@@ -2,6 +2,16 @@
 {
     public class Algoritm
     {
+        public int CountDivisible(int A, int B, int K)
+        {
+            int[] result = new int[(B+1)-A];
+            int index = 0;
 
+            for (int i = A; i < B + 1; i++)
+            {
+                index = i % K == 0 ? index +1 : index + 0;
+            }
+            return index; 
+        }
     }
 }
