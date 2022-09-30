99ba0b1d222c6c4353dcaee642baeccb57b8365a
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-09-29 20:31:05 -0500

    Mejorando Performance de Algoritm

--- Leccion5SumasDePrefijo/CountDiv/CountDivLibrary/Algoritm.cs
+++ Leccion5SumasDePrefijo/CountDiv/CountDivLibrary/Algoritm.cs
@@ -1,17 +1,12 @@
-﻿namespace CountDivLibrary
+﻿using System.Xml.Schema;
+
+namespace CountDivLibrary
 {
     public class Algoritm
     {
         public int CountDivisible(int A, int B, int K)
         {
-            int[] result = new int[(B+1)-A];
-            int index = 0;
-
-            for (int i = A; i < B + 1; i++)
-            {
-                index = i % K == 0 ? index +1 : index + 0;
-            }
-            return index; 
+            return B/K - A/K + (A%K == 0 ? 1 : 0);
         }
     }
 }
