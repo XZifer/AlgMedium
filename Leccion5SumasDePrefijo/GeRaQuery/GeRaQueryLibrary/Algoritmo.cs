087a13ca4c9a7f11d64dfebecd5a197bb19938a7
Luis Fernando Sanchez Barrera <lic.luisfsanchezbarrera@gmail.com>
2022-09-30 19:48:57 -0500

    Agregando metodo FactMin()

--- Leccion5SumasDePrefijo/GeRaQuery/GeRaQueryLibrary/Algoritmo.cs
+++ Leccion5SumasDePrefijo/GeRaQuery/GeRaQueryLibrary/Algoritmo.cs
@@ -2,6 +2,33 @@
 {
     public class Algoritmo
     {
-
+        public int[] FactMin(string S, int[] P, int[] Q)
+        {
+            int index = 0;
+            string subs;
+            int[] factsImp = new int[P.Length];
+            for (int i = 0; i < P.Length; i++)
+            {
+                subs = S.Substring(P[i], Q[i] - P[i] +1);
+                if (subs.Contains("A"))
+                {
+                    factsImp[index] = 1;
+                }
+                 else if (subs.Contains("C"))
+                {
+                    factsImp[index] = 2;
+                }
+                else if (subs.Contains("G"))
+                {
+                    factsImp[index] = 3;
+                }
+                else
+                {
+                    factsImp[index] = 4;
+                }
+                index++;
+            }
+            return factsImp;
+        }
     }
 }
